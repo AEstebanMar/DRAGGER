175a4b381061a6cacf9884af3dd60c752f3f4dd3
Álvaro Esteban Martos <alvaroesteban@uma.es>
2023-06-08 15:49:07 +0200

    Output now deleted upon new run

--- DAGGER.sh
+++ DAGGER.sh
@@ -11,12 +11,16 @@
 
 echo -e "\nLaunching DAGGER 0.7\n============================================================================================"
 
+rm -r output
+mkdir -p output output/tables output/plots temp
+
+
 export pval=0.001 ### The p-value cutoff for the GWAS data. Default is 0.001
 Qcutoff=5 ### The top percentaje Q-value that will be selected from the GTEx data. Default is 5.
 
 cd ./src
 
-#./preprocessing.sh
+./preprocessing.sh
 ./Analysis.R $Qcutoff
 ./stats_plots.R
 
