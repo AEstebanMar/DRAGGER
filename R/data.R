a4fa44865046ce622fa2f8d28329a2823e3920af
Álvaro Esteban Martos <alvaroesteban@uma.es>
2023-10-26 16:18:02 +0200

    Added GWAS_demo item to data, and how to obtain it to data-raw. Created man and vignettes, began package documentation (as of now, only GWAS_demo is documented)

---
+++ R/data.R
@@ -0,0 +1,14 @@
+#' GWAS demo dataset
+#'
+#' A very small Alzheimer's Diseasevariant risk association taken from The GWAS
+#' Catalog via the gwasrapidd package
+#'
+#' @format ## `GWAS_demo`
+#' A data frame with 6 rows and 3 columns:
+#' \describe {
+#'	\item{variant_id}{Reference SNP ID}
+#'	\item{pvalue}{Statistical significance of SNP association to AD risk}
+#'	\item{beta_number}{Beta coefficient of SNP association to AD. Positive}
+#' }
+#' @source <Detailed in data-raw/GWAS_demo.R>
+"GWAS_demo"
---
+++ data-raw/GWAS_demo.R
@@ -0,0 +1,13 @@
+## code to prepare `GWAS_demo` dataset
+
+GWAS_demo <- gwasrapidd::get_associations(study_id = "GCST001951")
+
+associations <- data.frame(GWAS_demo@associations)
+dec_betas <- which(associations$beta_direction=="decrease")
+associations$beta_number[dec_betas] <- associations$beta_number[dec_betas] * -1
+
+risk_alleles <- data.frame(GWAS_demo@risk_alleles)
+
+GWAS_demo <- merge(risk_alleles, associations, by = "association_id")[, c("variant_id", "pvalue", "beta_number")]
+
+usethis::use_data(GWAS_demo, overwrite = TRUE)
