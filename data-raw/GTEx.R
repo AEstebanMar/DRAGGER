ea8ba42fde9d3d233b7f19090917a0c9d8e7adc7
Álvaro Esteban Martos <alvaroesteban@uma.es>
2023-10-27 14:15:00 +0200

    GTEx dataset massively reduced, unnecessary columns no longer saved. Also removed stringr dependency, simple operation in base R was enough

--- data-raw/GTEx.R
+++ data-raw/GTEx.R
@@ -4,8 +4,16 @@
 # https://www.gtexportal.org/home/downloads/adult-gtex#qtl. File GTEx_Analysis_v8_eQTL.tar.
 # Pre-processing detailed in GTEx.sh
 
-GTEx <- read.table ("Merged_eQTL.txt", header=TRUE, sep="", dec=".", fill = TRUE)
-GTEx <- cbind(stringr::str_split_fixed(GTEx$file_name.gene_id, ".v8.egenes.txt,", 2), GTEx[, -1])
+GTEx <- read.table ("Merged_eQTL.txt", header=TRUE,
+					sep="", dec=".", fill = TRUE)[, c("file_name.gene_id",
+					"gene_name", "strand", "variant_id", "tss_distance",
+					"rs_id_dbSNP151_GRCh38p7","slope","qval")]
+					
+split_first_col <- do.call(rbind,
+						strsplit(GTEx$file_name.gene_id, ".v8.egenes.txt,"))
+
+GTEx <- cbind(split_first_col, GTEx[, -1])
+
 colnames(GTEx)[1:2] <- c("Tissue","gene_id")
 
 usethis::use_data(GTEx, overwrite = TRUE)
