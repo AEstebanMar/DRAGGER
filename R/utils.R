af75e5f1e4c5a4b56862f33441e7b606099b48ad
Álvaro Esteban Martos <alvaroesteban@uma.es>
2023-11-02 13:46:38 +0100

    normalise_column function decomposed. Renamed to parse_column_names, now calls unexported .rename column function, which takes a data frame, pattern to match in column search, new column name, and max number of matches. Calls said function for p-value, rs ID, gene ID and drug name

--- R/utils.R
+++ R/utils.R
@@ -1,32 +1,38 @@
 
-normalise_columns <- function(df) {
-	pval_column <- grep("pval|p-val", colnames(df), ignore.case=TRUE)
-	rs_column <- grep("rs", colnames(df), ignore.case=TRUE)
-	gene_id_column <- grep("rs", colnames(df), ignore.case=TRUE)
-	drug_name_column <- grep("drug.*name", colnames(df), ignore.case=TRUE)
-	if (length(pval_column) != 0) {
-		message('P-value column found. Setting name to "pvalue"')
-		colnames(df)[pval_column] <- "pvalue"
+.rename_column <- function(df, expr, new_name, max_matches = Inf) {
+	message(paste('Matching', new_name, "column..."))
+	columns <- grep(expr, colnames(df), ignore.case=TRUE)
+	if (length(columns) == 0) {
+		message('No matches found')
 	}
-	if (length(rs_column) != 0) {
-		message('RS ID column found. Setting name to "rs_id"')
-		colnames(df)[rs_column] <- "rs_id"
-	} 
-	if (length(gene_id_column) != 0) {
-		message('Gene ID column found. Setting name to "gene_id"')
-		colnames(df)[gene_id_column] <- "gene_id"
+
+	if (length(columns) == 1) {
+		message('Match found. Parsing...')
+		colnames(df)[columns] <- new_name
 	}
-	if (length(drug_name_column) > 1) {
-		message('Interactions dataset has multiple drug name columns. Column
-			with the most unique names has been chosen')
-		drug_columns <- df[, drug_name_column]
-		uniques <- lapply(X = drug_columns, FUN = function(x) length(unique(x)))
+
+	if (length(columns) > 1) {
+		message("WARNING")
+		warning('Multiple matches found. Choosing column with the most
+			unique names for standardising')
+		uniques <- lapply(X = df[columns], FUN = function(x) length(unique(x)))
 		longest <- which.max(unlist(uniques))
-		colnames(df)[longest] <- "drug_name"
+		colnames(df)[columns[longest]] <- new_name
 	}
-	if (length(drug_name_column) == 1) {
-		message('Drug name column found. Setting name to "drug_name"')
-		colnames(df)[drug_name_column] <- "drug_name"
+
+	if (length(columns) > max_matches) {
+		error('Matches exceed max allowed value for column type. Please
+			choose one and remove or rename the others (e.g por expression data,
+			choose p-value for strengh of variant-expression association')
 	}
+
 	return(df)
 }
+
+parse_column_names <- function(df) {
+	df <- .rename_column(df, "pval|p-val", "pvalue", 1)
+	df <- .rename_column(df, "rs", "rs_id")
+	df <- .rename_column(df, "gene.*id", "gene_id")
+	df <- .rename_column(df, "drug.*name", "drug_name")
+	return(df)
+}	
