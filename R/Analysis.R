715d1c1f0bb1a71131454f7343693862cfb141f7
Álvaro Esteban Martos <alvaroesteban@uma.es>
2023-11-07 15:42:21 +0100

    Removed rest of old script in Analysis.R. Moved remove_duplicate_rs and filter_significance functions to R/input_filter.R. Documented both functions

--- R/Analysis.R
+++ R/Analysis.R
@@ -1,37 +1,6 @@
 
 ### FUNCTIONS
 
-remove_duplicate_rs <- function(df) {
-
-	if(!is.null(df$p_value)) {
-		message("Sorting input by statistical significance")
-		df <- df[order(df$p_value), ]
-	}
-	if(is.null(df$rs_id)) {
-		stop('RS ID column not found or not properly parsed. Please run input
-			through DAGGER::parse_column_names()')
-	}
-
-	if(!any(duplicated(df$rs_id))) {
-		return(df)
-	} else {
-		message("Duplicates found in dataset, choosing first occurrence")
-		res <- df[!duplicated(df$rs_id), ]
-		return(res)
-	}		
-}
-
-filter_significance <- function(df, value = 0.05) {
-	if(is.null(df$p_value)) {
-		message("No statistical significance column found in input or improperly
-			parsed. You might want to run it through DAGGER::parse_column_names
-			first. Returning it as-is")
-		return(df)
-	}
-	res <- df[df$p_value <= value, ]
-	return(res)
-}
-
 predict_effect <- function(gene_variant_df) {
 	# Logic: negative betas mean protection, positive betas mean risk.
 	# Negative slopes mean lower expression, positive slopes mean higher 
@@ -93,107 +62,7 @@ merge_gene_var_drug <- function(GWAS, GTEx, DGIdb) {
 
 DAGGER <- function(GWAS, GTEx, DGIdb) {
 	merged <- merge_gene_var_drug(GWAS = GWAS, GTEx = GTEx, DGIdb = DGIdb)
-
-	
 	DAGGER_df <- predict_effect(merged)
 	res <- get_candidates(DAGGER_df)
 	return(res)
 }
-
-### Drug analysis. A random analysis is also performed, removing coincidences for the results. This reduces the likelihood
-### of the found drugs having side effects, an important step on the priorization. Future versions of DAGGER will include
-### an option to disable this step if a larger drugs list is desired.
-
-
-# # If the "Betas" value matches the "Slopes" value, it means a protector SNP increasing expression
-# # levels or a risk SNP decreasing them has been found. In both cases, an activator is desired.
-
-# Interaction [Interaction == TRUE] = "activator"
-
-# # If they do NOT match, the inverse situations are true. Thus, an inhibitor is desired.
-
-# Interaction [Interaction == FALSE] = "inhibitor"
-
-# Recommended = Interaction == Pharma$interaction_types
-
-# # Cases where the known interaction is not "activator" or "inhibitor", a question mark is introduced.
-# # Deciding if said drugs are appropriate requires further analysis, currently not included in DAGGER.
-
-# Recommended [!Pharma$interaction_types %in% c("inhibitor","activator")] = "?"
-
-# RecPharma = cbind (Pharma, Interaction, Recommended)
-
-# DruggableTargets = length(unique(RecPharma$gene_name))
-
-# Candidates = RecPharma[Recommended==TRUE,]
-
-# TreatableTargets = length(unique(Candidates$gene_name))
-
-# message('Filtering complete!')
-
-# ### Found drugs are compared with Alzforum Therapeutics Alzheimer Disease table to check for already tested drugs.
-	
-# message('\nRemoving Alzforum matches...')
-
-# # Unification of name formats. First word is selected, as we will consider entries such as "Metformin" and
-# # "Metformin Hydroclorate" to be the same drug.
-
-# CandidatesName = toupper(stringr::str_split_fixed(Candidates$drug_claim_primary_name, " ", 2)[,1])
-
-# AlzforumName = toupper(stringr::str_split_fixed(Alzforum$Name, " ", 2)[,1])
-
-# # Elimination of special characters, which are problematic.
-
-# CandidatesName = gsub ( "[^0-9A-Za-z///' ]" , "'" , CandidatesName)
-
-# AlzforumName = gsub ( "[^0-9A-Za-z///' ]" , "'" , AlzforumName)
-
-# # Apostrophes are removed.
-
-# CandidatesName = gsub ( "'" , "" , CandidatesName)
-
-# AlzforumName = gsub ( "'" , "" , AlzforumName)
-
-# # This approach has a limitation. Drugs with names such as "Compund 31" or "EGFR inhibitor" are not properly considered
-# # in Candidates-Alzforum matching. However, such names are assigned to little-known drugs, which are yet to be tested
-# # for any disease and therefore will not have a match. In any case, they are in very small number, making manual
-# # confirmation very easy.
-
-# New = !CandidatesName%in%AlzforumName
-
-# NewCandidates = Candidates[New,]
-
-# write.table(NewCandidates, row.names = FALSE, sep = "\t", "Results.tsv")
-
-# message('Done!')
-
-# message('\nAnalysis complete!\n')
-
-# setwd('../')
-
-# cat("\nTotal GTEx Genes:\n", file="Summary.txt", append = TRUE)
-# cat(TotalGenes, file="Summary.txt", append = TRUE)
-# cat("\nTotal GTEx eQTL:\n", file="Summary.txt", append = TRUE)
-# cat(TotalEQTL, file="Summary.txt", append = TRUE)
-# cat("\nQ-value cutoff:\n", file="Summary.txt", append = TRUE)
-# cat(Qcutoff, file="Summary.txt", append = TRUE)
-# cat(" %\n", file="Summary.txt", append = TRUE)
-# cat("Filtered genes:\n", file="Summary.txt", append = TRUE)
-# cat(TopGenes, file="Summary.txt", append = TRUE)
-# cat("\nFiltered GTEx eQTL:\n", file="Summary.txt", append = TRUE)
-# cat(TopEQTL, file="Summary.txt", append = TRUE)
-# cat("\nMax Q-value:\n", file="Summary.txt", append = TRUE)
-# cat(TopQval[nrow(TopQval),"qval"], file="Summary.txt", append = TRUE)
-# cat("\nSignificant eQTLs:\n", file="Summary.txt", append = TRUE)
-# cat(matchedSigPols, file="Summary.txt", append = TRUE)
-# cat("\nRandom eQTLs:\n", file="Summary.txt", append = TRUE)
-# cat(matchedRandPols, file="Summary.txt", append = TRUE)
-# cat("\nFinal eQTLs:\n", file="Summary.txt", append = TRUE)
-# cat(DepEQTL, file="Summary.txt", append = TRUE)
-# cat("\nTotal potential targets:\n", file="Summary.txt", append = TRUE)
-# cat(PotentialTargets, file="Summary.txt", append = TRUE)
-# cat("\nTotal druggable targets:\n", file="Summary.txt", append = TRUE)
-# cat(DruggableTargets, file="Summary.txt", append = TRUE)
-# cat("\nTotal treatable targets:\n", file="Summary.txt", append = TRUE)
-# cat(TreatableTargets, file="Summary.txt", append = TRUE)
-
---
+++ R/input_filter.R
@@ -0,0 +1,65 @@
+
+#' Remove duplicate RS in GWAS dataframe
+#' 
+#' `remove_duplicate_rs` removes rows corresponding to duplicate rs, keeping
+#' the first occurrence. Dataframe is first sorted by p-value if column exists.
+#' @param df A DAGGER-parsed data frame of GWAS information. Must contain an
+#' rs_id column. p_value column optional, but recommended.
+#' @returns A data frame with no duplicate RS. Only the first occurrence of each
+#' duplicate will remain, or the most statistically significant if p-value
+#' column is found'
+#' @examples
+#' data(GWAS_demo)
+#' dupes_example <- data.frame(
+#'							rs_id=c("rs2710888", "rs182532", "rs9660106"),
+#' 							pvalue=c(2e-58, 1e-17, 2e-12),
+#' 							beta_number = example_df$beta_number[1:3] + 0.002
+#'							)
+#' dupes_example <- rbind(head(GWAS_demo), dupes_example)
+#' dupes_example <- parse_column_names(dupes_example)
+#' print(dupes_example)
+#' print(remove_duplicate_rs(dupes_example))
+
+remove_duplicate_rs <- function(df) {
+
+	if(!is.null(df$p_value)) {
+		message("Sorting input by statistical significance")
+		df <- df[order(df$p_value), ]
+	}
+	if(is.null(df$rs_id)) {
+		stop('RS ID column not found or not properly parsed. Please run input
+			through DAGGER::parse_column_names()')
+	}
+
+	if(!any(duplicated(df$rs_id))) {
+		return(df)
+	} else {
+		message("Duplicates found in dataset, choosing first occurrence")
+		res <- df[!duplicated(df$rs_id), ]
+		return(res)
+	}		
+}
+
+#' Filter dataframe by p-value
+#' 
+#' `filter_significance` filters out all rows in a data frame below input
+#' p-value (default 0.05).
+#' @param df A DAGGER-parsed data frame.
+#' @returns A subset of the original data frame with all rows passing filter.
+#' @examples
+#' data(GWAS_demo)
+#' example_df <- parse_column_names(head(GWAS_demo))
+#' print(example_df)
+#' example_df <- filter_significance(example_df, 1e-20)
+#' print(example_df)
+
+filter_significance <- function(df, value = 0.05) {
+	if(is.null(df$p_value)) {
+		message("No statistical significance column found in input or improperly
+			parsed. You might want to run it through DAGGER::parse_column_names
+			first. Returning it as-is")
+		return(df)
+	}
+	res <- df[as.integer(df$p_value) <= value, ]
+	return(res)
+}
