4c5e397a14cc6da42d0aba5628ff357a7f95b4af
Álvaro Esteban Martos <alvaroesteban@uma.es>
2023-11-07 16:17:50 +0100

    Documented merge_gene_var_drug function

--- R/merge_gene_var_drug.R
+++ R/merge_gene_var_drug.R
@@ -1,3 +1,21 @@
+#' Merge expression, GWAS and gene-drug interaction databases
+#' 
+#' `merge_gene_var_drug` takes three dataframes, one of GWAS, one of gene
+#' expression and one of gene-drug interaction information and merges them.
+#' It calls `remove_duplicate_rs`, `filter_significance` and
+#' `parse_column_names` as needed. GWAS and GTEx dataframes are merged
+#' by rs_id column, result is merged with DGIdb by gene_symbol column'
+#' @param GWAS,GTEx,DGIdb GWAS, gene expression and drug-gene interaction
+#' dataframes.
+#' @returns Merged dataframe.
+#' @examples
+#' GWAS_example <- GWAS_demo[501:511, ]
+#' GTEx_example <- GTEx[c(1:5, 1598, 146580, 219755), ]
+#' DGIdb_example <- DGIdb[c((10:14), 5307, 5853, 8511), ]
+#' merged <- merge_gene_var_drug(GWAS_example, GTEx_example, DGIdb_example)
+#' print(merged_example)
+#' @export
+
 merge_gene_var_drug <- function(GWAS, GTEx, DGIdb) {
 
 	message('Parsing variant data')
