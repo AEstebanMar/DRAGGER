0e7e9e221cf8d1cad71e40f6c8d203416660412f
Álvaro Esteban Martos <alvaroesteban@uma.es>
2023-11-08 12:56:49 +0100

    Split comment line, exceeded 80 characters

--- data-raw/GTEx.R
+++ data-raw/GTEx.R
@@ -1,8 +1,8 @@
 ## R code to prepare `GTEx` dataset
 
 # Merged_eQTL.txt comes from bash processing of GWAS dataset downloaded from
-# https://www.gtexportal.org/home/downloads/adult-gtex#qtl. File GTEx_Analysis_v8_eQTL.tar.
-# Pre-processing detailed in GTEx.sh
+# https://www.gtexportal.org/home/downloads/adult-gtex#qtl.
+# File GTEx_Analysis_v8_eQTL.tar. Pre-processing detailed in GTEx.sh
 
 GTEx <- read.table ("Merged_eQTL.txt", header=TRUE,
 					sep="", dec=".", fill = TRUE)[, c("file_name.gene_id",
